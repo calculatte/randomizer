7354a6c9af9e454f5fa5a9bec701a66fb895c0da
calculatte <258551230+calculatte@users.noreply.github.com>
2026-02-02 01:20:34 -0500

    clean store file

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,4 @@
-import {type Writable, writable} from "svelte/store";
+import { type Writable, writable } from "svelte/store";
 import type { CarbonTheme } from "carbon-components-svelte/src/Theme/Theme.svelte";
 
 export const theme: Writable<CarbonTheme> = writable<CarbonTheme>("g10");
